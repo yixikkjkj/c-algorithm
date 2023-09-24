0fc2faef33cfcf6a6deffe3caf08a0ed178aeccd
yixikkjkj <wyjnext@qq.com>
2023-09-24 22:00:15 +0800

    array

--- array.c
+++ array.c
@@ -3,29 +3,46 @@
 typedef struct {
     int *nums;
     unsigned int size;
+    unsigned int max_size;
 } array;
 
-int randomAccess(array arr, unsigned int size) {
-    int index = rand() % size;
-    return arr.nums[index];
-}
-
-void push(array arr, int num) {
-    arr.nums[arr.size] = num;
-    arr.size += 1;
-}
-
-void insert(array arr, int num, unsigned int index) {
-    for (unsigned int i = arr.size - 1; i > index; i --) {
-        arr.nums[i] = arr.nums[i - 1];
+void _extend(array *arr, unsigned int new_max_size) {
+    int *res = malloc(sizeof(int) * new_max_size);
+    for (unsigned int i = 0; i < arr->size; i++) {
+        res[i] = arr->nums[i];
     }
-    arr.nums[index] = num;
+    free(arr->nums);
+    arr->nums = res;
+    arr->max_size = new_max_size;
 }
 
-void remove(array arr, unsigned int index) {
-    for (unsigned int i = index; i < arr.size - 1; i ++) {
-        arr.nums[i] = arr.nums[i + 1];
+void push(array *arr, int num) {
+    if (arr->size >= arr->max_size) {
+        _extend(arr, arr->max_size ? arr->max_size * 2 : 8);
     }
+    arr->nums[arr->size] = num;
+    arr->size += 1;
+}
+
+void insert(array *arr, int num, unsigned int index) {
+    if (arr->size >= arr->max_size) {
+        _extend(arr, arr->max_size * 2);
+    }
+    for (unsigned int i = arr->size - 1; i > index; i --) {
+        arr->nums[i] = arr->nums[i - 1];
+    }
+    arr->nums[index] = num;
+}
+
+void remove(array *arr, unsigned int index) {
+    if (index >= arr->size) {
+        return;
+    }
+    for (unsigned int i = index; i < arr->size - 1; i++)
+    {
+        arr->nums[i] = arr->nums[i + 1];
+    }
+    arr->size -= 1;
 }
 
 int find(array arr, int num) {
@@ -37,15 +54,9 @@ int find(array arr, int num) {
     return -1;
 }
 
-void extend(array arr, unsigned int new_size) {
-    int *res = malloc(sizeof(int) * new_size);
-    for (unsigned int i = 0; i < arr.size; i++) {
-        res[i] = arr.nums[i];
-    }
-    for (unsigned int i = arr.size; i < new_size; i++) {
-        res[i] = 0;
-    }
-    free(arr.nums);
-    arr.nums = res;
-    arr.size = new_size;
+int main() {
+    array arr = {NULL, 0, 0};
+    push(&arr, 1);
+    remove(&arr, 0);
+    return 1;
 }
