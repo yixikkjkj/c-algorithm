57b390d405f66709a7e964dfcb3895950ff94a32
wangyijun <wangyijun@meideng.net>
2023-10-09 13:22:35 +0800

    linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -7,7 +7,7 @@ typedef struct {
 } LinkedNode;
 
 typedef struct {
-    LinkedNode *head;
+    LinkedNode *guard;
     unsigned int size;
 } LinkedList;
 
@@ -19,21 +19,85 @@ LinkedNode *new_linked_node(int val) {
 }
 
 LinkedList new_linked_list() {
-    LinkedList list = {NULL, 0};
+    LinkedNode *guard = new_linked_node(0);
+    guard->next = guard;
+    LinkedList list = {guard, 0};
     return list;
 }
 
 void push(LinkedList *list, int val) {
     LinkedNode *node = new_linked_node(val);
-    if (list->head == NULL) {
-        list->head = node;
-        list->size = 1;
+    LinkedNode *curr = list->guard;
+    for (unsigned int i = 0; i < list->size; i++) {
+        curr = curr->next;
+    }
+    list->size++;
+    LinkedNode *next = curr->next;
+    curr->next = node;
+    node->next = next;
+}
+
+void insert(LinkedList *list, int val, unsigned int index) {
+    if (index >= list->size) {
         return;
     }
-    LinkedNode *current = list->head;
-    while (current->next != NULL) {
-        current = current->next;
+    LinkedNode *node = new_linked_node(val);
+    LinkedNode *curr = list->guard;
+    for (unsigned int i = 0; i < index; i++) {
+        curr = curr->next;
     }
-    current->next = node;
     list->size++;
+    LinkedNode *next = curr->next;
+    curr->next = node;
+    node->next = next;
+}
+
+void remove(LinkedList *list, unsigned int index) {
+    if (index >= list->size) {
+        return;
+    }
+    LinkedNode *node = list->guard;
+    for (unsigned int i = 0; i < index; i++) {
+        node = node->next;
+    }
+    // 要移除的就是下一个node
+    LinkedNode *rm_node = node->next;
+    node->next = rm_node->next;
+    list->size--;
+    free(rm_node);
+}
+
+void print_list(LinkedList *list) {
+    LinkedNode *node = list->guard->next;
+    for (unsigned int i = 0; i < list->size; i++) {
+        printf("%d ", node->val);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+LinkedNode *access(LinkedList *list, unsigned int index) {
+    if (index >= list->size) {
+        return NULL;
+    }
+    LinkedNode *node = list->guard;
+    for (unsigned int i = 0; i <= index; i++) {
+        node = node->next;
+    }
+    return node;
+}
+
+int main() {
+    LinkedList list = new_linked_list();
+    push(&list, 1);
+    push(&list, 2);
+    push(&list, 3);
+    print_list(&list);
+    insert(&list, 4, 0);
+    print_list(&list);
+    remove(&list, 1);
+    print_list(&list);
+    LinkedNode *node = access(&list, 1);
+    printf("%d \n", node->val);
+    return 0;
 }
